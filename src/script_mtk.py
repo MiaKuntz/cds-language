7d5f4e70e984adcb24df09c75891876af0fab4fb
MiaKuntz <mia.t.k@hotmail.com>
2023-03-08 10:57:46 +0100

    changes

---
+++ src/script_mtk.py
@@ -0,0 +1,15 @@
+# load packages
+import spacy
+
+# initialize spaCy
+nlp = spacy.load("en_core_web_lg")
+
+# defining main function
+def main():
+    text = "This is a string"
+    doc = nlp(text)
+    for token in doc:
+        print(token.text)
+
+if __name__=="__main__":
+    main()
