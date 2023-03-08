1022f4994f4acc3575a573ee6d27f79d07d45293
MiaKuntz <mia.t.k@hotmail.com>
2023-03-08 20:15:06 +0100

    worked on nbs6

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # create virtual enviroment
-python3 -m venv spacy_env
+#python3 -m venv spacy_env
 
 # activate virtual enviroment
-source ./spacy_env/bin/active
+#source ./spacy_env/bin/active
 
 # then install requirements
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-python3 -m spacy download en_core_web_md
+#python3 -m spacy download en_core_web_md
 
 # deactivate the env
-deactivate
+#deactivate
