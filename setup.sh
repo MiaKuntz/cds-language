96975838df8ee5da90fd9accd6ccdd64a5235fa6
MiaKuntz <mia.t.k@hotmail.com>
2023-03-29 10:17:37 +0200

    nbs9 work

--- setup.sh
+++ setup.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-# create virtual enviroment
-#python3 -m venv spacy_env
-
-# activate virtual enviroment
-#source ./spacy_env/bin/active
-
-# then install requirements
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-#python3 -m spacy download en_core_web_md
-
-# deactivate the env
-#deactivate
 # install hdbscan for BERTopic
 sudo apt-get update
 sudo apt-get install python3-dev
